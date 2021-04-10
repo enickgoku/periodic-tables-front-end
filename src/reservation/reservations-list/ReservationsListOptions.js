16cfb3e8e234cb897c8ea5c811c260c8345bf544
Jon Belaire <jon.belaire@icloud.com>
2021-04-09 21:38:19 -0400

    added tooltips

--- src/reservation/reservations-list/ReservationsListOptions.js
+++ src/reservation/reservations-list/ReservationsListOptions.js
@@ -7,6 +7,8 @@ import Col from "react-bootstrap/Col";
 import Dropdown from "react-bootstrap/Dropdown";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Button from "react-bootstrap/Button";
+import Tooltip from "react-bootstrap/Tooltip";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 /**
  * Defines the `Button` options for the `ReservationsListHeader` component.
@@ -30,73 +32,109 @@ function ReservationsListOptions(props) {
         <Row className="p-3">
             <Col xs={12} className="d-flex justify-content-between p-0">
                 <ButtonGroup className="w-100 mr-3">
-                    <Button
-                        variant="dark"
-                        className="border border-list-bg"
-                        style={{ fontSize: "1.2rem" }}
-                        onClick={() => changeDate(-1)}
+                    <OverlayTrigger
+                        transition={false}
+                        placement="top"  
+                        overlay={<Tooltip id="reservations-previous-tooltip">Previous</Tooltip>}
                     >
-                        <i className="ri-arrow-left-s-line" />
-                    </Button>
-                    <Button
-                        variant="dark"
-                        className="border border-list-bg"
-                        style={{ fontSize: "1.2rem" }}
-                        onClick={() => setDateSetting(currentDate)}
-                    >
-                        <i className="ri-home-line" />
-                    </Button>
-                    <Button
-                        variant="dark"
-                        className="border border-list-bg"
-                        style={{ fontSize: "1.2rem" }}
-                        onClick={() => changeDate(1)}
-                    >
-                        <i className="ri-arrow-right-s-line" />
-                    </Button>
-                </ButtonGroup>
-                <ButtonGroup>
-                    <Button
-                        variant="dark"
-                        className="border border-list-bg"
-                        style={{ fontSize: "1.2rem" }}
-                        onClick={() => reloadReservationList()}
-                    >
-                        <i className="ri-refresh-line" />
-                    </Button>
-                    <Button
-                        variant="dark"
-                        className="border border-list-bg"
-                        style={{ fontSize: "1.2rem" }}
-                        href={`/reservations/new`}
-                    >
-                        <i className="ri-add-line" />
-                    </Button>
-                    <Dropdown as={ButtonGroup}>
-                        <Dropdown.Toggle
+                        <Button
                             variant="dark"
                             className="border border-list-bg"
                             style={{ fontSize: "1.2rem" }}
+                            onClick={() => changeDate(-1)}
                         >
-                            <i className="ri-filter-3-line" />
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu align="right">
-                            <Dropdown.Header>Filter</Dropdown.Header>
-                            <Dropdown.Item onSelect={() => setReservationsFilter("all")}>
-                                All
-                            </Dropdown.Item>
-                            <Dropdown.Item onSelect={() => setReservationsFilter("booked")}>
-                                Booked
-                            </Dropdown.Item>
-                            <Dropdown.Item onSelect={() => setReservationsFilter("seated")}>
-                                Seated
-                            </Dropdown.Item>
-                            <Dropdown.Header>History</Dropdown.Header>
-                            <Dropdown.Item onSelect={() => setReservationsFilter("finished")}>
-                                Finished
-                            </Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
+                            <i className="ri-arrow-left-s-line" />
+                        </Button>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        transition={false}
+                        placement="top"  
+                        overlay={<Tooltip id="reservations-current-tooltip">Today</Tooltip>}
+                    >
+                        <Button
+                            variant="dark"
+                            className="border border-list-bg"
+                            style={{ fontSize: "1.2rem" }}
+                            onClick={() => setDateSetting(currentDate)}
+                        >
+                            <i className="ri-home-line" />
+                        </Button>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        transition={false}
+                        placement="top"  
+                        overlay={<Tooltip id="reservations-next-tooltip">Next</Tooltip>}
+                    >
+                        <Button
+                            variant="dark"
+                            className="border border-list-bg"
+                            style={{ fontSize: "1.2rem" }}
+                            onClick={() => changeDate(1)}
+                        >
+                            <i className="ri-arrow-right-s-line" />
+                        </Button>
+                    </OverlayTrigger>
+                </ButtonGroup>
+                <ButtonGroup>
+                    <OverlayTrigger
+                        transition={false}
+                        placement="top"  
+                        overlay={<Tooltip id="reservations-refresh-tooltip">Refresh</Tooltip>}
+                    >
+                        <Button
+                            variant="dark"
+                            className="border border-list-bg"
+                            style={{ fontSize: "1.2rem" }}
+                            onClick={() => reloadReservationList()}
+                        >
+                            <i className="ri-refresh-line" />
+                        </Button>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        transition={false}
+                        placement="top"  
+                        overlay={<Tooltip id="reservations-add-tooltip">Add</Tooltip>}
+                    >
+                        <Button
+                            variant="dark"
+                            className="border border-list-bg"
+                            style={{ fontSize: "1.2rem" }}
+                            href={`/reservations/new`}
+                        >
+                            <i className="ri-add-line" />
+                        </Button>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        transition={false}
+                        placement="top"  
+                        overlay={<Tooltip id="reservations-filter-tooltip">Filter</Tooltip>}
+                    >
+                        <Dropdown as={ButtonGroup}>
+                            <Dropdown.Toggle
+                                variant="dark"
+                                className="border border-list-bg"
+                                style={{ fontSize: "1.2rem" }}
+                            >
+                                <i className="ri-filter-3-line" />
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu align="right">
+                                <Dropdown.Header>Filter</Dropdown.Header>
+                                <Dropdown.Item onSelect={() => setReservationsFilter("all")}>
+                                    All
+                                </Dropdown.Item>
+                                <Dropdown.Item onSelect={() => setReservationsFilter("booked")}>
+                                    Booked
+                                </Dropdown.Item>
+                                <Dropdown.Item onSelect={() => setReservationsFilter("seated")}>
+                                    Seated
+                                </Dropdown.Item>
+                                <Dropdown.Header>History</Dropdown.Header>
+                                <Dropdown.Item onSelect={() => setReservationsFilter("finished")}>
+                                    Finished
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </OverlayTrigger>
                 </ButtonGroup>
             </Col>
         </Row>
--- src/table/tables-list/TablesListOptions.js
+++ src/table/tables-list/TablesListOptions.js
@@ -5,6 +5,8 @@ import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Button from "react-bootstrap/Button";
+import Tooltip from "react-bootstrap/Tooltip";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 /**
  * Defines the `ButtonGroup` options for the `TablesListHeader` component.
@@ -26,14 +28,25 @@ function TablesListOptions(props) {
 
     return (
         <ButtonGroup>
-            <Button
-                variant="dark"
-                className="border border-list-bg"
-                style={{ fontSize: "1.2rem" }}
-                onClick={() => refreshFilteredTables()}
+            <OverlayTrigger
+                transition={false}
+                placement="top"  
+                overlay={<Tooltip id="tables-refresh-tooltip">Refresh</Tooltip>}
             >
-                <i className="ri-refresh-line" />
-            </Button>
+                <Button
+                    variant="dark"
+                    className="border border-list-bg"
+                    style={{ fontSize: "1.2rem" }}
+                    onClick={() => refreshFilteredTables()}
+                >
+                    <i className="ri-refresh-line" />
+                </Button>
+            </OverlayTrigger>
+            <OverlayTrigger
+                transition={false}
+                placement="top"  
+                overlay={<Tooltip id="tables-add-tooltip">Add</Tooltip>}
+            >
             <Button
                 variant="dark"
                 className="border border-list-bg"
@@ -42,27 +55,34 @@ function TablesListOptions(props) {
             >
                 <i className="ri-add-line" />
             </Button>
-            <Dropdown as={ButtonGroup}>
-                <Dropdown.Toggle
-                    variant="dark"
-                    className="border border-list-bg"
-                    style={{ fontSize: "1.2rem" }}
-                >
-                    <i className="ri-filter-3-line" />
-                </Dropdown.Toggle>
-                <Dropdown.Menu align="right">
-                    <Dropdown.Header>Filter</Dropdown.Header>
-                    <Dropdown.Item onSelect={() => setFilter("all")}>
-                        All
-                    </Dropdown.Item>
-                    <Dropdown.Item onSelect={() => setFilter("free")}>
-                        Free
-                    </Dropdown.Item>
-                    <Dropdown.Item onSelect={() => setFilter("occupied")}>
-                        Occupied
-                    </Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
+            </OverlayTrigger>
+            <OverlayTrigger
+                transition={false}
+                placement="top"  
+                overlay={<Tooltip id="tables-filter-tooltip">Filter</Tooltip>}
+            >
+                <Dropdown as={ButtonGroup}>
+                    <Dropdown.Toggle
+                        variant="dark"
+                        className="border border-list-bg"
+                        style={{ fontSize: "1.2rem" }}
+                    >
+                        <i className="ri-filter-3-line" />
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu align="right">
+                        <Dropdown.Header>Filter</Dropdown.Header>
+                        <Dropdown.Item onSelect={() => setFilter("all")}>
+                            All
+                        </Dropdown.Item>
+                        <Dropdown.Item onSelect={() => setFilter("free")}>
+                            Free
+                        </Dropdown.Item>
+                        <Dropdown.Item onSelect={() => setFilter("occupied")}>
+                            Occupied
+                        </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </OverlayTrigger>
         </ButtonGroup>
     );
 }
